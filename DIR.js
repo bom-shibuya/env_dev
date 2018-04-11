e8763b03cd3e5c8d196da5d28af49de786cd2f7b
bom-shibuya <19891636+bom-shibuya@users.noreply.github.com>
2018-04-11 17:48:55 +0900

    :wrench: DirectoryManager.js => DIR.js

---
+++ DIR.js
@@ -0,0 +1,11 @@
+module.exports = path => {
+  const basePath = path || '';
+  return {
+    SRC: basePath + 'app/src/',
+    SRC_ASSETS: basePath + 'app/src/assets/',
+    DEST: basePath + 'app/dest/',
+    DEST_ASSETS: basePath + 'app/dest/assets/',
+    RELEASE: basePath + 'app/_release/',
+    RELEASE_ASSETS: basePath + 'app/_release/assets/'
+  };
+};
